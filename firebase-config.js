58168ca702634fd665e591c12606d62cc4fe1d76
avivvasani <vasaniaviv@gmail.com>
2025-08-15 14:28:33 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -3,7 +3,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyAmwb7V54q-nZK_5y-vm_i345iSXyrFZmc",
   authDomain: "mockstock-5c5e3.firebaseapp.com",
   projectId: "mockstock-5c5e3",
-  storageBucket: "mockstock-5c5e3.firebasestorage.app",
+  storageBucket: "mockstock-5c5e3.firebase-config.appspot.com",
   messagingSenderId: "480136918782",
   appId: "1:480136918782:web:393d8ec0099e866c9617af",
   measurementId: "G-VJHD474WRZ"
